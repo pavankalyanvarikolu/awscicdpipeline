44dfd0c43618a43b51b886ab8e7f9e71d7dd10af
pavan kalyan <p.kalyanvarikolu@gmail.com>
2024-05-02 20:10:55 +0530

    init the repo and pushing the code to the github

---
+++ src/main/java/com/cicdpipeline/awscicdpipeline/AwscicdpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.cicdpipeline.awscicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscicdpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscicdpipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicdpipeline/awscicdpipeline/AwscicdpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdpipeline.awscicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscicdpipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
